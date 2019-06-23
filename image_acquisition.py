0c2e8650854d87ff0aa26f72a8365b1e60219f49
Abhinav Patel <abhinavpatel2912@gmail.com>
2019-06-24 02:11:51 +0530

    Update image_acquisition.py

--- image_acquisition.py
+++ image_acquisition.py
@@ -1,41 +1,39 @@
+# Importing Libraries
+
 import sys
 import json
 import os
 import numpy as np
 
-<<<<<<< HEAD
-global_min_x = 100000
-global_max_x = 0
-
-global_min_y = 100000
-global_max_y = img_height
+# Default video frame dimensions
 
 img_height = 240
 img_width = 320
 
-# min - margin_offset and max + margin_offset this is for x-coord
-# min - margin_offset for y-coord
-=======
-img_height = 240
-img_width = 320
-
-
-# min - margin_offset and max + margin_offset : for x-coord
-# min - margin_offset : for y-coord
->>>>>>> 6a4f24e92c3d0b893a413dcfce745119c7e6ae28
-
-margin_offset = 25  # in pixels
-
+offset = 25  # amount of shift in extreme points (in pixels)
 
 def getExtrmCoords(points_list):
+
+    '''
+
+    :param points_list: list of x and y coordinates of joints along with their confidence scores
+    :return: extreme coordinates in x and y directions
+    
+    '''
+
     x_coord = []
     y_coord = []
     print(points_list)
     for ptr in range(0, len(points_list), 3):
 
-        print("ptr is {} and len(points_list) is {}".format(ptr, len(points_list)))
-        x_coord.append(points_list[ptr])
-        y_coord.append(points_list[ptr + 1])
+        # print("ptr is {} and len(points_list) is {}".format(ptr, len(points_list)))
+        curr_x = points_list[ptr]
+        curr_y = points_list[ptr + 1]
+        
+        if curr_x >= 0 and curr_x <= img_width:
+            x_coord.append(curr_x)
+        if curr_y >= 0 and curr_y <= img_height:
+            y_coord.append(curr_y)
 
     print(x_coord)
     print(y_coord)
@@ -50,7 +48,16 @@ def getExtrmCoords(points_list):
 
 
 
-def getBoundingBox(json_path):
+def getBoundingBox(json_path, global_val):
+    
+    '''
+    
+    :param json_path: path of directory containing all the json files of the respective video
+    :param global_val: list of variables storing extreme coordinates
+    :return: bounding box around the person in the given video
+    '''
+
+    global_min_x, global_max_x, global_min_y, global_max_y = global_val
 
     file_list = os.listdir(json_path)
 
@@ -76,33 +83,36 @@ def getBoundingBox(json_path):
         pose = main_dict['pose_keypoints']
         hand_left = main_dict['hand_left_keypoints']
         hand_right = main_dict['hand_right_keypoints']
-        face = main_dict['face_keypoints']
+
+        # face = main_dict['face_keypoints']
 
         pose_min_x, pose_max_x, pose_min_y = getExtrmCoords(pose)
         hand_left_min_x, hand_left_max_x, hand_left_min_y = getExtrmCoords(hand_left)
         hand_right_min_x, hand_right_max_x, hand_right_min_y = getExtrmCoords(hand_right)
-        face_min_x, face_max_x, face_min_y = getExtrmCoords(face)
+        # face_min_x, face_max_x, face_min_y = getExtrmCoords(face)
 
 
-        global_min_x = min(global_min_x, pose_min_x, hand_left_min_x, hand_right_min_x, face_min_x)
-        global_max_x = min(global_max_x, pose_max_x, hand_left_max_x, hand_right_max_x, face_max_x)
+        global_min_x = min(global_min_x, pose_min_x, hand_left_min_x, hand_right_min_x)
+        global_max_x = max(global_max_x, pose_max_x, hand_left_max_x, hand_right_max_x)
 
-        global_min_y = min(global_min_y, pose_min_y, hand_left_min_y, hand_right_min_y, face_min_y)
+        # global_min_y = min(global_min_y, pose_min_y, hand_left_min_y, hand_right_min_y, face_min_y)
 
-    if global_min_x - margin_offset <= 0:
+    print("global_min_x is {} and global_max_x is {}".format(global_min_x, global_max_x))
+
+    if global_min_x - offset <= 0:
         global_min_x = 0
     else:
-        global_min_x -= margin_offset
+        global_min_x -= offset
 
-    if global_max_x + margin_offset > img_width:
+    if global_max_x + offset > img_width:
         global_max_x = img_width
     else:
-        global_max_x += margin_offset
+        global_max_x += offset
 
-    if global_min_y - margin_offset <= 0:
-        global_min_y = 0
-    else:
-        global_min_y -= margin_offset
+    # if global_min_y - offset <= 0:
+    #     global_min_y = 0
+    # else:
+    #     global_min_y -= offset
 
 
     bounding_box = [
@@ -115,31 +125,41 @@ def getBoundingBox(json_path):
     return bounding_box
 
 
-def main():
-    """
-        returns bounding box around the person for each video using its json files
-    """
-    store_json = "/home/axp798/axp798gallinahome/json2/"
+def main(global_val):
+    '''
+    
+    :param global_val: list of variables storing extreme coordinates
+    :return: bounding box for each of the video of ChaLearn Dataset
+    '''
 
-    # dir_list = os.listdir(store_json)
-    # for dir_name in dir_list:
-    #     video_list = os.listdir(os.path.join(store_json, dir_name))
+    store_json = "/home/axp798/axp798gallinahome/store/json/"
 
-    #     for video_name in video_list:
-    #         json_path = os.path.join(store_json, "{}/{}/".format(dir_name, video_name))
+    dir_list = os.listdir(store_json)
+    for dir_name in dir_list:
+        video_list = os.listdir(os.path.join(store_json, dir_name))
 
-    #         bounding_box = getBoundingBox(json_path)
+        for video_name in video_list:
+            json_path = os.path.join(store_json, "{}/{}/".format(dir_name, video_name))
 
-    #         return bounding_box
+            bounding_box = getBoundingBox(json_path)
 
-    print("started!")
-    bounding_box = getBoundingBox(store_json)
+            return bounding_box
 
-    print(bounding_box)
+    # print("started!")
+    # bounding_box = getBoundingBox(store_json, global_val)
+    # 
+    # print(bounding_box)
 
 if __name__ == '__main__':
+    
+    global_min_x = 100000
+    global_max_x = 0
 
+    global_min_y = 0
+    global_max_y = img_height
 
-    main()
+    global_val = [global_min_x, global_max_x, global_min_y, global_max_y]
+
+    main(global_val)
 
 
