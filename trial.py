cf46d6e6279d2c25b67d5d6279e3f91a13b0a1b5
Abhinav Patel <abhinavpatel2912@gmail.com>
2019-07-22 02:38:27 -0400

    update file

--- trial.py
+++ trial.py
@@ -1,4 +1,4 @@
-# import image_acquisition2 as img_ac
+import image_acquisition2 as img_ac
 import os
 import pickle
 
@@ -18,6 +18,7 @@ global_val = [global_min_x, global_max_x, global_min_y, global_max_y]
 data = []
 
 num_videos = 0
+lines = []
 
 with open(labels_path, 'r') as f:
     lines = f.readlines()
@@ -36,47 +37,38 @@ for curr_line in lines:
     print("num_videos: {}".format(num_videos))
 
     print("************** ################# ********************")
-#
-# with open(labels_path, 'r') as f:
-#     curr_line = f.readline()
-#     line_ele = curr_line.split(' ')
-#     dir_info = line_ele[0]
-#
-#     par_name, vid_name = dir_info.split('/')[0], dir_info.split('/')[1]
-#     label_info = line_ele[1:]
-#
-#     for curr_label in label_info:
-#         curr_label_info = curr_label.split(':')
-#         range = curr_label_info[0]
-#         label = curr_label_info[1]
-#
-#         start_frame = range.split(',')[0]
-#         end_frame = range.split(',')[1]
-#
-#         start, end = int(start_frame), int(end_frame)
-#
-#         # Get bounding box
-#         dir_path = os.path.join(json_store_path, par_name)
-#         vid_path = os.path.join(dir_path, vid_name)
-#         if not os.path.exists(dir_path) or os.path.exists(vid_path):
-#             continue
-#
-#         json_path = vid_path
-#         bounding_box = img_ac.getBoundingBox(json_path, global_val, start, end)
-#
-#         curr_dict = {}
-#         curr_video_path = os.path.join(videos_path, dir_info)
-#
-#         curr_dict.update({"video_dir": curr_video_path})
-#         curr_dict.update({"range_of_frames": [start, end]})
-#         curr_dict.update({"label": label})
-#         curr_dict.update({"bounding_box": bounding_box})
-#
-#         data.append(curr_dict)
-#
-#
-# with open('bounding_box_train.pkl', 'wb') as f:
-#     pickle.dump(data, f)
-#
+
+    for curr_label in label_info:
+        curr_label_info = curr_label.split(':')
+        range = curr_label_info[0]
+        label = curr_label_info[1]
+
+        start_frame = range.split(',')[0]
+        end_frame = range.split(',')[1]
+
+        start, end = int(start_frame), int(end_frame)
+
+        # Get bounding box
+        dir_path = os.path.join(json_store_path, par_name)
+        vid_path = os.path.join(dir_path, vid_name)
+        if not os.path.exists(dir_path) or os.path.exists(vid_path):
+            continue
+
+        json_path = vid_path
+        bounding_box = img_ac.getBoundingBox(json_path, global_val, start, end)
+
+        curr_dict = {}
+        curr_video_path = os.path.join(videos_path, dir_info)
+
+        curr_dict.update({"video_dir": curr_video_path})
+        curr_dict.update({"range_of_frames": [start, end]})
+        curr_dict.update({"label": label})
+        curr_dict.update({"bounding_box": bounding_box})
+
+        data.append(curr_dict)
+
+with open('bounding_box_train.pkl', 'wb') as f:
+    pickle.dump(data, f)
+
 #
 #
