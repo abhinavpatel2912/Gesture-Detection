d4d7753fe37634517a4940d06a31d012ef21c852
Abhinav Patel <abhinavpatel2912@gmail.com>
2019-09-08 13:47:43 -0400

    made changes

--- image_acquisition.py
+++ image_acquisition.py
@@ -6,7 +6,6 @@ import os
 import numpy as np
 
 # Default video frame dimensions
-<<<<<<< HEAD
 
 img_height = 240
 img_width = 320
@@ -20,21 +19,6 @@ def getExtrmCoords(points_list):
     :param points_list: list of x and y coordinates of joints along with their confidence scores
     :return: extreme coordinates in x and y directions
 
-=======
-
-img_height = 240
-img_width = 320
-
-offset = 25  # amount of shift in extreme points (in pixels)
-
-def getExtrmCoords(points_list):
-
-    '''
-
-    :param points_list: list of x and y coordinates of joints along with their confidence scores
-    :return: extreme coordinates in x and y directions
-    
->>>>>>> 87803fcc53a05e4692baba2fd44429cc36b7b2a2
     '''
 
     x_coord = []
@@ -42,7 +26,6 @@ def getExtrmCoords(points_list):
     print(points_list)
     for ptr in range(0, len(points_list), 3):
 
-        # print("ptr is {} and len(points_list) is {}".format(ptr, len(points_list)))
         curr_x = points_list[ptr]
         curr_y = points_list[ptr + 1]
         
@@ -63,17 +46,9 @@ def getExtrmCoords(points_list):
         return points_min_x, points_max_x, points_min_y
 
 
-
 def getBoundingBox(json_path, global_val):
-<<<<<<< HEAD
-
-    '''
-
-=======
     
     '''
-    
->>>>>>> 87803fcc53a05e4692baba2fd44429cc36b7b2a2
     :param json_path: path of directory containing all the json files of the respective video
     :param global_val: list of variables storing extreme coordinates
     :return: bounding box around the person in the given video
@@ -149,7 +124,6 @@ def getBoundingBox(json_path, global_val):
 
 def main(global_val):
     '''
-<<<<<<< HEAD
 
     :param global_val: list of variables storing extreme coordinates
     :return: bounding box for each of the video of ChaLearn Dataset
@@ -168,51 +142,15 @@ def main(global_val):
 
             return bounding_box
 
-    # print("started!")
-    # bounding_box = getBoundingBox(store_json, global_val)
-    #
-=======
-    
-    :param global_val: list of variables storing extreme coordinates
-    :return: bounding box for each of the video of ChaLearn Dataset
-    '''
-
-    store_json = "/home/axp798/axp798gallinahome/store/json/"
-
-    dir_list = os.listdir(store_json)
-    for dir_name in dir_list:
-        video_list = os.listdir(os.path.join(store_json, dir_name))
-
-        for video_name in video_list:
-            json_path = os.path.join(store_json, "{}/{}/".format(dir_name, video_name))
-
-            bounding_box = getBoundingBox(json_path)
-
-            return bounding_box
-
-    # print("started!")
-    # bounding_box = getBoundingBox(store_json, global_val)
-    # 
->>>>>>> 87803fcc53a05e4692baba2fd44429cc36b7b2a2
-    # print(bounding_box)
 
 if __name__ == '__main__':
-    
+
     global_min_x = 100000
     global_max_x = 0
 
     global_min_y = 0
     global_max_y = img_height
 
-<<<<<<< HEAD
-    global_min_x = 100000
-    global_max_x = 0
-
-    global_min_y = 0
-    global_max_y = img_height
-
-=======
->>>>>>> 87803fcc53a05e4692baba2fd44429cc36b7b2a2
     global_val = [global_min_x, global_max_x, global_min_y, global_max_y]
 
     main(global_val)
